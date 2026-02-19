2f54b5bd2593155b51c20cf9ed27e7de8e8b6d3b
TeaGodSS <TeaGodSS@users.noreply.github.com>
2026-02-19 00:59:20 +0000

    deploy: 23d5fa05d552b79c9a8250fb24a58fb939607154

---
+++ _app/immutable/chunks/CL5oOlH3.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1gz7oig)==null?void 0:s.base)??"/magium-recrystallized";var a;const t=((a=globalThis.__sveltekit_1gz7oig)==null?void 0:a.assets)??e;export{t as a,e as b};
